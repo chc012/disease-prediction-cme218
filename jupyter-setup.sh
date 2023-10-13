c56ec64b612ff49311b8b824815417a8fee72706
Cheng Chang <chc012@stanford.edu>
2023-10-12 21:49:27 -0700

    separate session and jupyter alloc

--- jupyter-setup.sh
+++ jupyter-setup.sh
@@ -1,12 +1,3 @@
-# Resource allocation
-# Use this list if you want to change setting
-# -c      number of CPU cores to request (OpenMP/pthreads, default: 1)
-# -g      number of GPUs to request (default: none)
-# -m      memory amount to request (default: 4GB)
-# -p      partition to run the job in (default: dev)
-# -t      time limit (default: 01:00:00)
-sdev -c 4 -m 16GB -t 02:00:00
-
 # Get the useful libraries
 ml python/3.9.0
 ml py-numpy/1.24.2_py39
---
+++ session-alloc.sh
@@ -0,0 +1,8 @@
+# Resource allocation
+# Use this list if you want to change setting
+# -c      number of CPU cores to request (OpenMP/pthreads, default: 1)
+# -g      number of GPUs to request (default: none)
+# -m      memory amount to request (default: 4GB)
+# -p      partition to run the job in (default: dev)
+# -t      time limit (default: 01:00:00)
+sdev -c 4 -m 16GB -t 02:00:00
