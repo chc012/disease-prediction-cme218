c211e7f591e2a2482674a267cfe8c766e1c8e041
Cheng Chang <chc012@stanford.edu>
2023-10-12 23:20:45 -0700

    switch to pip setup

--- jupyter-setup.sh
+++ jupyter-setup.sh
@@ -1,17 +1,17 @@
-# Get the useful libraries
-ml python/3.9.0
-ml py-pandas/2.0.1_py39
-ml py-matplotlib/3.7.1_py39
-ml py-seaborn/0.12.1_py39
-ml py-scikit-learn/1.0.2_py39
-ml py-scipy/1.10.1_py39
-ml py-ipython/8.3.0_py39
-ml py-jupyter/1.0.0_py39
-ml py-pytorch/2.0.0_py39
-ml py-optuna/2.10.0_py39
+# # Get the useful libraries
+# ml python/3.9.0
+# ml py-pandas/2.0.1_py39
+# ml py-matplotlib/3.7.1_py39
+# ml py-seaborn/0.12.1_py39
+# ml py-scikit-learn/1.0.2_py39
+# ml py-scipy/1.10.1_py39
+# ml py-ipython/8.3.0_py39
+# ml py-jupyter/1.0.0_py39
+# ml py-pytorch/2.0.0_py39
+# ml py-optuna/2.10.0_py39
 
 # pip things that are not module
-pip install tqdm
+pip install -r requirements.txt
 
 # start jupyter
 jupyter notebook --no-browser --port=8888
